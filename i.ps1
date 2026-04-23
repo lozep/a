d23779641a5065d6f99f14a6d52ed714b4453ca2
lozep <letvenoreg@gmail.com>
2026-04-23 19:13:35 +0300

    Update i.ps1

--- i.ps1
+++ i.ps1
@@ -1,38 +1,33 @@
 Add-Type -AssemblyName System.Windows.Forms
-
 $url = "https://raw.githubusercontent.com/lozep/a/refs/heads/main/l.png"
 $path = "$env:TEMP\i.png"
-
 Invoke-WebRequest -Uri $url -OutFile $path
 $code = @'
 using System;
 using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-public class WindowsUtils {
+public class Wallpaper {
     [DllImport("user32.dll", CharSet = CharSet.Auto)]
     public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-
-    public static async Task SetAccountPicture(string imagePath) {
-        var file = await Windows.Storage.StorageFile.GetFileFromPathAsync(imagePath);
-        await Windows.System.UserProfile.UserInformation.SetAccountPicturesAsync(file, file, file);
-    }
 }
 '@
-Add-Type -TypeDefinition $code
-[WindowsUtils]::SystemParametersInfo(0x0014, 0, $path, 0x01 -bor 0x02)
+if (-not ([System.Management.Automation.PSTypeName]"Wallpaper").Type) {
+    Add-Type -TypeDefinition $code
+}
+[Wallpaper]::SystemParametersInfo(0x0014, 0, $path, 0x01 -bor 0x02)
 $shell = New-Object -ComObject Shell.Application
 $shell.MinimizeAll()
 $title = " 0_0 "
 $msg = "$([char]1090+[char]1099+[char]32+[char]1079+[char]1072+[char]32+[char]1086+[char]1074+[char]1072+[char]1083+[char]1100+[char]1085+[char]1086+[char]1075+[char]1086+[char]63+[char]63+[char]63+[char]63+[char]63)"
 $result = [System.Windows.Forms.MessageBox]::Show($msg, $title, [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Warning)
 if ($result -eq [System.Windows.Forms.DialogResult]::No) {
-    $url = "https://raw.githubusercontent.com/lozep/a/refs/heads/main/n.jpg"
+    $urlAvatar = "https://raw.githubusercontent.com/lozep/a/refs/heads/main/n.jpg"
     $fileName = "$([char]0x041F)$([char]0x0420)$([char]0x0418)$([char]0x0412)$([char]0x0415)$([char]0x0422).jpg"
     $tempPath = Join-Path $env:TEMP $fileName
     try {
-        Invoke-WebRequest -Uri $url -OutFile $tempPath
-        $task = [WindowsUtils]::SetAccountPicture($tempPath)
-        $task.Wait()
+        Invoke-WebRequest -Uri $urlAvatar -OutFile $tempPath
+        $asTask = [Windows.System.UserProfile.UserInformation, Windows.System.UserProfile, ContentType=WindowsRuntime]::SetAccountPicturesAsync
+        $file = [Windows.Storage.StorageFile, Windows.Storage, ContentType=WindowsRuntime]::GetFileFromPathAsync($tempPath).GetAwaiter().GetResult()
+        [Windows.System.UserProfile.UserInformation, Windows.System.UserProfile, ContentType=WindowsRuntime]::SetAccountPicturesAsync($file, $file, $file).GetAwaiter().GetResult()
         $destinations = @(
             [Environment]::GetFolderPath("MyDocuments"),
             "$env:USERPROFILE\Downloads",
@@ -48,6 +43,6 @@ if ($result -eq [System.Windows.Forms.DialogResult]::No) {
         }
         Remove-Item $tempPath
     } catch {
-        
+        # Тихо игнорируем ошибки, если API недоступно
     }
 }
